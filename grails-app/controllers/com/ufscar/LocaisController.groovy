bdfd0b12091ce97688970a4faf715af2e74cc78d
ThiagoYonamine <thyonamine1@gmail.com>
2017-06-24 18:35:24 -0300

    imagens2

---
+++ grails-app/controllers/com/ufscar/LocaisController.groovy
@@ -0,0 +1,115 @@
+package com.ufscar
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class LocaisController {
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def hi(){
+        def hobbies = ["basketball", "photography"]
+        render(view: "modeloLocal", model: [name: "Maricel", hobbies: hobbies])
+    }
+    def listar() {
+        def hobbies = Locais.list()
+        render(view: "modeloLocal", model: [name: "Maricel", hobbies: hobbies])
+    }
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Locais.list(params), model:[locaisCount: Locais.count()]
+    }
+
+    def show(Locais locais) {
+        respond locais
+    }
+
+    def create() {
+        respond new Locais(params)
+    }
+
+    @Transactional
+    def save(Locais locais) {
+        if (locais == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (locais.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond locais.errors, view:'create'
+            return
+        }
+
+        locais.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'locais.label', default: 'Locais'), locais.id])
+                redirect locais
+            }
+            '*' { respond locais, [status: CREATED] }
+        }
+    }
+
+    def edit(Locais locais) {
+        respond locais
+    }
+
+    @Transactional
+    def update(Locais locais) {
+        if (locais == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (locais.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond locais.errors, view:'edit'
+            return
+        }
+
+        locais.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'locais.label', default: 'Locais'), locais.id])
+                redirect locais
+            }
+            '*'{ respond locais, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Locais locais) {
+
+        if (locais == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        locais.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'locais.label', default: 'Locais'), locais.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'locais.label', default: 'Locais'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
---
+++ grails-app/domain/com/ufscar/Locais.groovy
@@ -0,0 +1,10 @@
+package com.ufscar
+
+class Locais {
+    String nome
+    String url
+    String descricao
+    String categoria
+    static constraints = {
+    }
+}
